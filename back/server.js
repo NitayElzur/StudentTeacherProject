2478e0a6597ace167f44faaf6eadf825d658b3de
Nitay Elzur <elzur.nitay@gmail.com>
2023-09-13 17:18:50 +0300

    socket reconnect config

--- back/server.js
+++ back/server.js
@@ -39,6 +39,14 @@ io.on('connection', socket => {
         console.log(socket.id + ' disconnected');
     })
 
+    socket.on('reconnect', () => {
+        console.log(`A client reconnected after ${attemptNumber} attempts`);
+    })
+
+    socket.on('reconnect_failed', () => {
+        console.log('A client failed to reconnect');
+    })
+
     socket.on('write', (value, room) => {
         socket.to(room).emit('write', value)
         console.log('update');
--- front/src/contexts/SocketContext.jsx
+++ front/src/contexts/SocketContext.jsx
@@ -2,7 +2,11 @@ import { createContext, useState } from "react";
 import { useEffect } from 'react';
 import { io } from 'socket.io-client'
 export const SocketContext = createContext();
-const socket = io(import.meta.env.VITE_HOST);
+const socket = io(import.meta.env.VITE_HOST, {
+    reconnection: true,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 30 * 1000
+});
 
 export const SocketProvider = ({ children }) => {
     const [role, setRole] = useState();
@@ -17,10 +21,22 @@ export const SocketProvider = ({ children }) => {
         socket.on('connect', () => {
             console.log('connected as ' + socket.id);
         })
+        socket.on('disconnect', () => {
+            console.log('disconnected');
+        })
+        socket.on('reconnect', (attemptNumber) => {
+            console.log(`Reconnected to the server after ${attemptNumber} attempts`);
+        })
+        socket.on('reconnect_failed', () => {
+            console.log('Failed to reconnect');
+        })
         return () => {
             socket.off('role');
             socket.off('connect-error');
             socket.off('connect');
+            socket.off('disconnect');
+            socket.off('reconnect');
+            socket.off('reconnect_failed');
         }
     }, [])
     return (
